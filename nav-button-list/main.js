e60f7d4f16fec2c6269a5111558d2bc63464abe2
Peter Wang <happypeter1983@gmail.com>
2013-09-22 21:38:02 +0800

    nav-button-list works

--- nav-button-list/main.js
+++ nav-button-list/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-  $('ul li').click(function () {
-    $('ul li').removeClass('current');
+  $('.content').children().hide();
+  $('.content').children('.first').show();
+  $('.nav-button-list ul li').click(function () {
+    $('.nav-button-list ul li').removeClass('current');
+    var myClass = $(this).attr("class");
+    $('.content').children().hide();
+    $('.content').children('.' + myClass).show();
     $(this).addClass('current');
     console.log($(this), 'clicked!!!')
   });
